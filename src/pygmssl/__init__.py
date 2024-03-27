65f77a043e005b03f6e2efb5054170490b6e0e15
Zhang Jie <zhangj_10ujs@foxmail.com>
2024-03-27 05:24:44 +0000

    set version

--- src/pygmssl/__init__.py
+++ src/pygmssl/__init__.py
@@ -12,5 +12,5 @@ def get_gmssl_version_num() -> str:
     return _gm.gmssl_version_num()
 
 
-VERSION = __version__ = '0.1.0'
+VERSION = __version__ = '0.1.1'
 __all__ = ['SM2', 'SM3', 'SM4', 'get_gmssl_version_str', 'get_gmssl_version_num', 'VERSION', '__version__']
