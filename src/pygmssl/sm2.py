bf00f82497b5e21e94d5b17067387dfd1efc5efd
Zhang Jie <13131481+j-z10@users.noreply.github.com>
2024-03-30 20:25:34 +0800

    add sm2 key pre-check before using it (#11)

--- src/pygmssl/sm2.py
+++ src/pygmssl/sm2.py
@@ -1,7 +1,9 @@
 import base64
 from ctypes import byref, c_uint8, c_size_t, Structure, c_char_p, pointer
+import functools
 import tempfile
 import os
+from typing import Callable, Literal, Concatenate, Self
 
 from Cryptodome.Util.asn1 import DerSequence
 
@@ -41,38 +43,63 @@ class _SM2_SIGN_CTX(Structure):
 class SM2:
     def __init__(self, pub_key: bytes | None = None, pri_key: bytes | None = None):
         self._sm2_key = _SM2_KEY()
+        self._has_pub = self._has_pri = False
         if pub_key and len(pub_key) == 65 and pub_key[0] == 4:
             # if 65 bytes, 0x04 + pub.x + pub.y
             pub_key = pub_key[1:]
         if pub_key:
-            if len(pub_key) != 64:
-                raise ValueError('the length of sm2 public key should be 64 bytes')
-            self._sm2_key.pub.x[:32] = pub_key[:32]
-            self._sm2_key.pub.y[:32] = pub_key[32:64]
+            self.set_pub(pub_key)
         if pri_key:
-            if len(pri_key) != 32:
-                raise ValueError('the length of sm2 private key should be 32 bytes')
-            self._sm2_key.pri[:32] = pri_key
+            self.set_pri(pri_key)
+
+    def set_pub(self, pub_key: bytes):
+        if len(pub_key) != 64:
+            raise ValueError('the length of sm2 public key should be 64 bytes')
+        self._sm2_key.pub.x[:32] = pub_key[:32]
+        self._sm2_key.pub.y[:32] = pub_key[32:64]
+        self._has_pub = True
+
+    def set_pri(self, pri_key: bytes):
+        if len(pri_key) != 32:
+            raise ValueError('the length of sm2 private key should be 32 bytes')
+        self._sm2_key.pri[:32] = pri_key
+        self._has_pri = True
+
+    @staticmethod
+    def check(propery: Literal['_has_pri'] | Literal['_has_pub']):
+        def _func[**P, R](fn: Callable[Concatenate[Self, P], R]):
+            @functools.wraps(fn)
+            def wrapper(self: Self, *args: P.args, **kwargs: P.kwargs) -> R:
+                if not getattr(self, propery):
+                    raise ValueError(f'{propery} not set')
+                return fn(self, *args, **kwargs)
+            return wrapper
+        return _func
 
     @classmethod
     def generate_new_pair(cls) -> 'SM2':
         obj = cls()
         _gm.sm2_key_generate(byref(obj._sm2_key))
+        obj._has_pri = obj._has_pub = True
         return obj
 
     @property
-    def pub_key(self) -> bytes:
+    @check('_has_pub')
+    def pub_key(self: 'SM2') -> bytes:
         return bytes(self._sm2_key.pub)
 
     @property
+    @check('_has_pri')
     def pri_key(self) -> bytes:
         return bytes(self._sm2_key.pri)
 
+    @check('_has_pub')
     def compute_z(self, id: bytes = SM2_DEFAULT_ID) -> bytes:
         z = (c_uint8 * 32)()
         _gm.sm2_compute_z(byref(z), byref(self._sm2_key.pub), c_char_p(id), len(id))
         return bytes(z)
 
+    @check('_has_pri')
     def sign(self, data: bytes, id: bytes = SM2_DEFAULT_ID, asn1: bool = False) -> bytes:
         _sign_ctx = _SM2_SIGN_CTX()
         _gm.sm2_sign_init(byref(_sign_ctx), byref(self._sm2_key), c_char_p(id), len(id))
@@ -88,18 +115,18 @@ class SM2:
         if asn1:
             _k = DerSequence()
             _k.decode(sig)
-            r, s = _k[0], _k[1]
-            sig = r.to_bytes(32, 'big') + s.to_bytes(32, 'big')
+            sig = bytes(_k[0].to_bytes(32, 'big') + _k[1].to_bytes(32, 'big'))  # type: ignore
         return sig
 
+    @check('_has_pub')
     def verify(self, data: bytes, sig: bytes, id: bytes = SM2_DEFAULT_ID, asn1: bool = False) -> bool:
         if len(sig) == 64:
             if not asn1:
                 raise ValueError('when sig is 64 bytes, ans1 flag must be true')
             # asn1 der格式的, 通常是JAVA搞过来的
             _k = DerSequence()
-            _k.append(int.from_bytes(sig[:32], 'big'))
-            _k.append(int.from_bytes(sig[32:], 'big'))
+            _k.append(int.from_bytes(sig[:32], 'big'))  # type: ignore
+            _k.append(int.from_bytes(sig[32:], 'big'))  # type: ignore
             sig = _k.encode()
         _verify_ctx = _SM2_SIGN_CTX()
         _gm.sm2_verify_init(byref(_verify_ctx), byref(self._sm2_key), c_char_p(id), len(id))
@@ -111,6 +138,7 @@ class SM2:
         ret = _gm.sm2_verify_finish(byref(_verify_ctx), c_char_p(sig), len(sig))
         return ret == 1
 
+    @check('_has_pub')
     def encrypt(self, data: bytes) -> bytes:
         if len(data) > SM2_MAX_PLAINTEXT_SIZE:
             raise ValueError('to encrypt data\'s length must <= sm2.SM2_MIN_PLAINTEXT_SIZE')
@@ -121,6 +149,7 @@ class SM2:
         _gm.sm2_encrypt(byref(self._sm2_key), byref(buff), len(data), byref(out), byref(length))
         return bytes(out[:length.value])
 
+    @check('_has_pri')
     def decrypt(self, data: bytes) -> bytes:
         if len(data) > SM2_MAX_CIPHERTEXT_SIZE:
             raise ValueError('to decrypt data\'s length must <= sm2.SM2_MAX_CIPHERTEXT_SIZE')
@@ -193,6 +222,7 @@ class SM2:
             obj = SM2()
             assert _gm.sm2_private_key_info_decrypt_from_pem(byref(obj._sm2_key), c_char_p(password), fp) == 1
             libc.fclose(fp)
+            obj._has_pri = obj._has_pub = True
             return obj
 
     @classmethod
@@ -205,6 +235,7 @@ class SM2:
             obj = SM2()
             assert _gm.sm2_public_key_info_from_pem(byref(obj._sm2_key), fp) == 1
             libc.fclose(fp)
+            obj._has_pub = True
             return obj
 
     @staticmethod
@@ -231,6 +262,7 @@ class SM2:
         assert _gm.sm2_private_key_info_decrypt_from_der(byref(obj._sm2_key), byref(
             p), byref(attr_len), password, byref(cp), byref(buflen)) == 1
         assert buflen.value == 0
+        obj._has_pri = obj._has_pub = True
         return obj
 
     @classmethod
@@ -243,6 +275,7 @@ class SM2:
         cp = pointer(buf)
         assert _gm.sm2_public_key_info_from_der(byref(obj._sm2_key), byref(cp), byref(vlen)) == 1
         assert vlen.value == 0
+        obj._has_pub = True
         return obj
 
     if win32:
