7cff5ba21d892fa30b16a288036c5b338fb3395e
Zhang Jie <13131481+j-z10@users.noreply.github.com>
2024-10-09 22:32:19 +0800

    update dependances and dev container (#33)

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-git clone -b 'v3.1.1' --depth 1 https://github.com/guanzhi/GmSSL.git
-cd GmSSL && mkdir build && cd build && cmake ..
-make && make test && sudo make install
-sudo ldconfig
 
-# check gmssl installed
-gmssl version
+rm -rf ./.venv
+poetry install --sync --with dev,test
